9599d3b3d098001926ae43d55cfef948d3c1f2f9
terry.cho <bwcho75@gmail.com>
2013-05-27 00:20:38 +0900

    initial commit

---
+++ restapi/src/main/java/terry/restapi/dao/ContactDao.java
@@ -0,0 +1,18 @@
+package terry.restapi.dao;
+
+import terry.restapi.model.ContactVo;
+
+public class ContactDao {
+	public void create(ContactVo contact){
+		
+	}
+	public ContactVo get(String email){
+		return null;
+	}
+	public void delete(String email){
+		
+	}
+	public void update(String email,ContactVo contact){
+	
+	}
+}
---
+++ restapi/src/main/java/terry/restapi/service/ContactService.java
@@ -0,0 +1,33 @@
+package terry.restapi.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import terry.restapi.dao.ContactDao;
+import terry.restapi.model.ContactVo;
+
+@Path("/contact")
+public class ContactService {
+	ContactDao dao;
+	public void setContactDao(ContactDao dao){this.dao = dao;}
+	
+	@POST
+	public void create(ContactVo contact){
+		
+	}
+	@GET
+	@Produces("application/json")
+	@Path("{email}")
+	public ContactVo get(@PathParam("email") String email){
+		return dao.get(email);
+	}
+	@PUT
+	@Path("{email}")
+	public void update(@PathParam("email") String email, ContactVo contact){
+		dao.update(email, contact);
+	}
+}
